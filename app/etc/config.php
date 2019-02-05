a04cbbf51e09dea46cc3f10407ebe778ffc2a0f4
Koos van Egmond <koos.vanegmond@kega.nl>
2019-02-05 14:39:27 +0100

    - Added kega deploy dedicated

--- app/etc/config.php
+++ app/etc/config.php
@@ -5,6 +5,7 @@ return [
         'Magento_Directory' => 1,
         'Magento_Eav' => 1,
         'Magento_Theme' => 1,
+        'Kega_DeployDedicated' => 1,
         'Magento_Cms' => 1,
         'Magento_Customer' => 1,
         'Magento_AdminNotification' => 1,
